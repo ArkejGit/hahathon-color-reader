adf1239bf1cc260aecb447507b5b746b59b98991
ArkejGit <robert.krawczyk92@gmail.com>
2017-09-24 12:40:33 +0200

    Pull state upwards - from InputForm component to the main component

--- color-reader/src/InputForm.js
+++ color-reader/src/InputForm.js
@@ -1,52 +1,15 @@
 import React from 'react';
 
-export default class InputForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: ''};
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    if(this.handleValidation()){
-           console.log(`Color: ${this.state.value}`);
-        }else{
-           console.log("Invalid color format")
-        }
-  }
-
-  handleValidation() {
-    // regex from https://venturedevs.com/color-picker/index.html
-    let REGEX = {
-                HEX: /^#([a-fA-F0-9]{2})([a-fA-F0-9]{2})([a-fA-F0-9]{2})$/,
-                SHORT_HEX: /^#([a-fA-F0-9])([a-fA-F0-9])([a-fA-F0-9])$/,
-                RGB: /^rgb\((\d{1,3}),(\d{1,3}),(\d{1,3})\)$/,
-                HSL: /^hsl\((\d{1,3}),(\d{1,3})%,(\d{1,3})%\)$/
-            };
-    let value = this.state.value;
-
-    return value.match(REGEX.HEX) ||
-           value.match(REGEX.SHORT_HEX) ||
-           value.match(REGEX.RGB) ||
-           value.match(REGEX.HSL);
-  }
-
-  render() {
+const InputForm = (props) => {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <form onSubmit={props.onSubmit}>
         <label>
           Color:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
+          <input type="text" value={props.value} onChange={props.onChange} />
         </label>
         <input type="submit" value="Submit" />
       </form>
     );
-  }
 }
+
+export default InputForm;
--- color-reader/src/index.js
+++ color-reader/src/index.js
@@ -4,11 +4,49 @@ import InputForm from './InputForm.js';
 import './index.css';
 
 class ColorReader extends React.Component {
+  constructor() {
+    super();
+    this.state = {value: ''};
+  }
+
+  handleChange = e => {
+    this.setState({value: e.target.value});
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    if(this.handleValidation()){
+           console.log(`Color: ${this.state.value}`);
+        }else{
+           console.log("Invalid color format")
+        }
+  }
+
+  handleValidation() {
+    // regex from https://venturedevs.com/color-picker/index.html
+    let REGEX = {
+                HEX: /^#([a-fA-F0-9]{2})([a-fA-F0-9]{2})([a-fA-F0-9]{2})$/,
+                SHORT_HEX: /^#([a-fA-F0-9])([a-fA-F0-9])([a-fA-F0-9])$/,
+                RGB: /^rgb\((\d{1,3}),(\d{1,3}),(\d{1,3})\)$/,
+                HSL: /^hsl\((\d{1,3}),(\d{1,3})%,(\d{1,3})%\)$/
+            };
+    let value = this.state.value;
+
+    return value.match(REGEX.HEX) ||
+           value.match(REGEX.SHORT_HEX) ||
+           value.match(REGEX.RGB) ||
+           value.match(REGEX.HSL);
+  }
+
   render() {
     return (
       <div id="colorReader">
         <div id="inputWrapper">
-          <InputForm />
+          <InputForm 
+            value={this.state.value}
+            onChange={this.handleChange}
+            onSubmit={this.handleSubmit}
+          />
         </div>
         <div id="outputWrapper">
           <div className="colorFormat">
