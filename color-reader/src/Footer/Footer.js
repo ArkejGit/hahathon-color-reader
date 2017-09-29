70d8b1fac85e1332c088a4be4cc09a148d9035fa
ArkejGit <robert.krawczyk92@gmail.com>
2017-09-29 20:49:23 +0200

    Finish footer designing

--- color-reader/src/Footer/Footer.js
+++ color-reader/src/Footer/Footer.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import './Footer.css';
+import SO_logo from './img/so-icon.png';
+import GH_logo from './img/GitHub-Mark-32px.png';
 
 const Footer = (props) => {
     return (
       <div id="footer">
-      <div className="header">
+      <div id="footer-header">
       Hahathon
       </div>
-      <div className="content">
+      <div id="footer-content">
+      <div>
       created by ARKEJ
       </div>
+      <a href='https://github.com/ArkejGit'><img src={GH_logo} alt='GitHub'></img></a>
+      <a href='https://stackoverflow.com/users/5904606/arkej?tab=profile'><img src={SO_logo} alt='Stack Overflow'></img></a>
+      </div>      
       </div>
     );
 }
