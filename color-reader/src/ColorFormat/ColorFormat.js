eae2889b27bdafb123d335d4956ff3c3d5b740c7
ArkejGit <robert.krawczyk92@gmail.com>
2017-09-29 20:53:16 +0200

    Move some styling from ColorReader to ColorFormat

--- color-reader/src/ColorFormat/ColorFormat.js
+++ color-reader/src/ColorFormat/ColorFormat.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import DisplayBox from '../DisplayBox/DisplayBox.js';
 import Button from '../Button/Button.js';
+import './ColorFormat.css';
 
 const copyToClipboard = (text) => {
   window.prompt("Copy to clipboard: Ctrl+C, Enter", text);
